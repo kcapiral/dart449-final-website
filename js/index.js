df8c261821f13624a182594b13b79cd6aa8b2e25
Kathleen Capiral <Kathleen@Kathleens-MacBook-Pro.local>
2017-12-04 13:34:07 -0500

    before js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,17 @@ $(document).ready(function() {
 	window.addEventListener('wheel', function(e) {
 		changeSection(e.deltaY);
 	});
+
+	$('.toggleParent').click(function(){ 
+		$(this).next().toggle("slow");
+	});
 });
+
+function scrollToAnchor(anchor) {
+	$('html, body').animate({
+        scrollTop: $("#" + anchor).offset().top
+    }, 1000);
+}
 
 function changeSection(scrollDirection) {
 	if (scrollDirection > 0) {
